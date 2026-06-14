1dbb4f42db8bed6357e2edd6558abe227ddecc6a
Pittawat Samroengsin <>
2026-06-14 18:45:37 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,9 @@ export default defineNuxtConfig({
     cssPath: "~/assets/css/main.css",
     configPath: "tailwind.config.ts",
   },
+  image: {
+    provider: "none",
+  },
 
   app: {
     head: {
